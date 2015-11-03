b4b5aac3323d43a46ffd59d2b8c5dba52b6a2407
ry99 <ryan@ostrich-emulators.com>
2015-11-03 13:37:43 -0500

    finished (?) conversion script

--- scripts/qhacc-to-jfxhacc.sh
+++ scripts/qhacc-to-jfxhacc.sh
@@ -14,6 +14,7 @@ SPLITS=$TMPDIR/splits
 ACCOUNTS=$TMPDIR/accounts
 TRANS=$TMPDIR/entries
 JOURNALS=$TMPDIR/journals
+SPLITTRANS=$TMPDIR/splittrans
 
 
 $SQLITE3 $DB "SELECT a.id, a.parentid, a.name, a.openingbalance, t.description FROM account a JOIN accounttype t ON a.accounttypeid=t.id" > $ACCOUNTS
@@ -21,6 +22,8 @@ $SQLITE3 $DB "SELECT a.id, a.parentid, a.name, a.openingbalance, t.description F
 $SQLITE3 $DB "SELECT s.id, s.accountid, s.amount, r.description FROM split s JOIN reconcilestate r ON s.reconcilestateid=r.id" > $SPLITS
 
 $SQLITE3 $DB "SELECT t.id, t.num, t.date, t.payee, t.journalid FROM transentry t WHERE t.typeid=1" > $TRANS
+
+$SQLITE3 $DB "SELECT x.transactionid, x.splitid FROM trans_split x JOIN transentry t ON x.transactionid=t.id JOIN split s ON x.splitid=s.id WHERE t.typeid=1" > $SPLITTRANS
 
 $SQLITE3 $DB "SELECT j.id, j.name FROM journal j" > $JOURNALS
 
@@ -42,8 +45,7 @@ cat << END_TEXT
 <http://com.ostrich-emulators/jfxhacc#qhacc-export-$$> a jfxhacc:dataset .
 END_TEXT
 
-
-sed -e"s/\([^|]\).\(.*\)/j:qhacc-journal-\1 a jfxhacc:journal ; rdfs:label \"\2\" ./g" $JOURNALS
+sed -e"s/\([^|]\+\).\(.*\)/j:qhacc-journal-\1 a jfxhacc:journal ; rdfs:label \"\2\" ./g" $JOURNALS
 
 awk --field-separator \| '{printf("a:qhacc-account-%d a jfxhacc:account ; rdfs:label \"%s\" ; accounts:openingBalance \"%d\"^^xsd:int ; accounts:accountType jfxhacc:%s ",$1,$3,$4,tolower($5)); if( $2>0 ){ printf( "; accounts:parent a:qhacc-account-%d ", $2 ) } ; printf( ".\n" ) }' $ACCOUNTS
 
@@ -51,5 +53,23 @@ cat $SPLITS | sed -e"s/Reconciled$/RECONCILED/g" \
   -e "s/No$/NOT_RECONCILED/g" \
   -e"s/Cleared$/CLEARED/g" $SPLITS | \
   awk --field-separator \| '{printf( "s:qhacc-split-%d a jfxhacc:split ; splits:account a:qhacc-account-%d ; splits:value \"%d\"^^xsd:int ; splits:reconciled \"%s\" .\n",$1,$2,$3,$4)}'
+
+# create a payee's lookup
+declare -A PAYEES
+while read line; do
+  PAYEES["$line"]=${#PAYEES[@]}
+  name=$(echo $line|sed -e's/"/\\"/g')
+  echo "p:qhacc-payee-${PAYEES[$line]} a jfxhacc:payee ; rdfs:label \"$name\" ."
+done < <(cat $TRANS | cut -d\| -f4|sort -u)
+
+while read line; do
+  echo "$line" | awk --field-separator \| \
+  '{printf("t:qhacc-transaction-%d a jfxhacc:transaction ; trans:journal j:qhacc-journal-%s ; trans:date \"%s\"^^xsd:date ; ", $1,$5,$3); if( ""!=$2 ){ printf("trans:number \"%s\" ; ",$2) } }'
+  
+  payee=$(echo $line|cut -d\| -f4)
+  echo " trans:payee p:qhacc-payee-${PAYEES[$payee]} ."  
+done < $TRANS
+
+sed -e"s/\([^|]\+\).\(.*\)/t:qhacc-transaction-\1 trans:entry s:qhacc-split-\2 ./g" $SPLITTRANS
 
 rm -rf $TMPDIR
